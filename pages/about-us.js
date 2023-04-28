2568115ccbaca371ed1e981c5f2be8fd3f5c028d
evtomak <evtomak@iu.edu>
2023-04-28 11:57:54 -0400

    Pushed.

--- pages/about-us.js
+++ pages/about-us.js
@@ -10,7 +10,7 @@ const About = () => {
             <h4>This is a simple pizza service application that allows a customer to submit an online order. </h4>
             {/* eslint-disable-next-line react/no-unescaped-entities */}
             <h4> We have been able to utilize Postman to grab customer data, and you can find a customer's order history based on their customer ID number. </h4>
-            <h4>We also created a home page with a welcome message, a menu section, and ordering functionality.</h4>
+            <h4>We also created a home page with a welcome message, a customer section, and ordering functionality, as well as a tracking search.</h4>
             <h4>As for our architecture style, we evidently chose a service-based architecture.</h4>
             <h4>As for our design patterns, we implemented the decorator, factory, and adapter design patterns.</h4>
         </div>
